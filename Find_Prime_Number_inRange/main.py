076f7cbea97dbeb59f8830ddfb75ac2f63f36e20
Sujan Chandra Roy <sujan.007.ice@gmail.com>
2023-04-23 16:39:35 +0900

    Update main.py

--- Find_Prime_Number_inRange/main.py
+++ Find_Prime_Number_inRange/main.py
@@ -1,4 +1,4 @@
-# Write a program to display all prime numbers within a range
+# Write a python program to display all prime numbers within a range
 
 start_num= int(input("Please enter starting number="))
 end_num = int(input("Please enter ending number="))
