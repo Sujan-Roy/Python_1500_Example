d9808913c64b31485f52e94b7d0f6fda11be41fe
unknown <Sujan-Roy>
2023-04-28 13:32:59 +0900

    fixed the bug in main.py

--- File_Basic/main.py
+++ File_Basic/main.py
@@ -1,12 +1,11 @@
 import os
 
-current_file = os.path.realpath('10_File_io_Basic_Concepts.ipynb')  
+current_file = os.path.realpath('file3.txt')  
 print('current file: {}'.format(current_file))
-# Note: in .py files you can get the path of current file by __file__
+
 
 current_dir = os.path.dirname(current_file)  
 print('current directory: {}'.format(current_dir))
 # Note: in .py files you can get the dir of current file by os.path.dirname(__file__)
-
-data_dir = os.path.join(os.path.dirname(current_dir), 'data')
-print('data directory: {}'.format(data_dir))
+file_Open = open(current_file)
+print(file_Open.read())
